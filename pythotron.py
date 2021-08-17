f2a50f5c8aaaef539d0762d2686dc3da2de8ad2c
eyaler <eyalgruss@gmail.com>
2021-08-18 02:51:58 +0300

    added solo/rec exclusive modes

--- pythotron.py
+++ pythotron.py
@@ -41,13 +41,22 @@ q  Quit
 i  Initialize
 s  Solo on all tracks
 a  solo off All tracks
+d  solo exclusive mode toggle
 m  Mute on all tracks
 n  mute off all tracks
+o  mute Override all tracks toggle
 r  Record on all tracks
 e  record off all tracks
+t  record exclusive mode Toggle
 u  solo/mute/record off all tracks
-o  Overide mute on/off all tracks
 '''.strip().splitlines()
+
+solo_exclusive_text = 'SOLO EXCL'
+mute_override_text = 'MUTE OVER'
+record_exclusive_text = 'REC. EXCL'
+solo_exclusive_y = 0
+mute_override_y = 1
+record_exclusive_y = 2
 
 midi_in = rtmidi.MidiIn()
 midi_out = rtmidi.MidiOut()
@@ -73,8 +82,10 @@ def toggle_all(state_names, v):
 
 sinewaves = []
 def init():
-    global override_mute, show_help, volumes, controls, new_controls, states, new_states, sinewaves
-    override_mute = False
+    global mute_override, solo_exclusive, record_exclusive, show_help, volumes, controls, new_controls, states, new_states, sinewaves
+    mute_override = False
+    solo_exclusive = False
+    record_exclusive = False
     show_help = False
     volumes = {k: min_db for k in range(num_controls)}
     controls = {}
@@ -92,7 +103,7 @@ init()
 
 
 def loop(screen):
-    global override_mute, show_help, new_controls, new_states
+    global mute_override, solo_exclusive, record_exclusive, show_help, new_controls, new_states
     slider_size = screen.height // 2
     knob_size = min(screen.height // 2, max_knob_size)
     if knob_size % 2 == 0:
@@ -102,6 +113,8 @@ def loop(screen):
     while True:
         if screen.has_resized():
             raise ResizeScreenError('Screen resized')
+        last_solo_on = None
+        last_record_on = None
         while msg := midi_in.get_message():
             msg = msg[0][1:]
             k = int(msg[0])
@@ -110,8 +123,14 @@ def loop(screen):
                 new_controls[k] = v
             else:
                 for state_name in states:
-                    if 0 <= k - state_starts[state_name] < num_controls:
-                        new_states[state_name][k - state_starts[state_name]] = not states[state_name][k - state_starts[state_name]] if external_led_mode else v > 0
+                    knorm = k - state_starts[state_name]
+                    if 0 <= knorm < num_controls:
+                        new_states[state_name][knorm] = not states[state_name][knorm] if external_led_mode else v > 0
+                        if new_states[state_name][knorm]:
+                            if state_name == 's':
+                                last_solo_on = knorm
+                            elif state_name == 'r':
+                                last_record_on = knorm
                         break
 
         for k, v in new_controls.items():
@@ -119,18 +138,26 @@ def loop(screen):
                 sinewaves[k - knob_start].set_pitch(notes[k - knob_start] + (v * 2 / 127 - 1) * pitch_max_delta)
         controls.update(new_controls)
 
+        if last_solo_on and solo_exclusive:
+            toggle_all('s', False)
+            new_states['s'][last_solo_on] = True
+
+        if last_record_on and record_exclusive:
+            toggle_all('r', False)
+            new_states['r'][last_record_on] = True
+
         for state_name in new_states:
-            states[state_name].update(new_states[state_name])
             for k, v in new_states[state_name].items():
                 new_controls[slider_start + k] = controls[slider_start + k]
                 new_controls[knob_start + k] = controls[knob_start + k]
                 if external_led_mode:
                     send_msg(state_name, k, v)
+            states[state_name].update(new_states[state_name])
         new_states = {state_name: {} for state_name in states}
 
         for k in range(num_controls):
             volume = min_db
-            if (not any(states['s'].values()) or states['s'][k]) and not states['m'][k] and not override_mute:
+            if (not any(states['s'].values()) or states['s'][k]) and not states['m'][k] and not mute_override:
                 volume += controls[k] / 127 * (max_db - min_db)
             if volume != volumes[k]:
                 sinewaves[k].set_volume(volume)
@@ -183,8 +210,14 @@ def loop(screen):
                     attr=Screen.A_NORMAL if states['m'][knorm] else Screen.A_BOLD,
                     bg=bg)
 
-        if override_mute:
-            screen.print_at('MUTE', screen.width - 4, 0, colour=overlay_fg_color, attr=overlay_attr, bg=overlay_bg_color)
+        if solo_exclusive:
+            screen.print_at(solo_exclusive_text, screen.width - len(solo_exclusive_text), solo_exclusive_y, colour=overlay_fg_color, attr=overlay_attr, bg=overlay_bg_color)
+
+        if mute_override:
+            screen.print_at(mute_override_text, screen.width - len(mute_override_text), mute_override_y, colour=overlay_fg_color, attr=overlay_attr, bg=overlay_bg_color)
+
+        if record_exclusive:
+            screen.print_at(record_exclusive_text, screen.width - len(record_exclusive_text), record_exclusive_y, colour=overlay_fg_color, attr=overlay_attr, bg=overlay_bg_color)
 
         help_x = max(0, (screen.width - len(max(help_text, key=len))) // 2)
         help_y = max(0, (screen.height - len(help_text)) // 2)
@@ -192,7 +225,7 @@ def loop(screen):
             for i, line in enumerate(help_text):
                 screen.print_at(line, help_x, help_y + i, colour=overlay_fg_color, attr=overlay_attr, bg=overlay_bg_color)
 
-        need_screen_refresh = bool(new_controls) or override_mute or show_help
+        need_screen_refresh = bool(new_controls) or mute_override or solo_exclusive or record_exclusive or show_help
         new_controls = {}
         ev = screen.get_event()
         if isinstance(ev, KeyboardEvent):  # note: Hebrew keys assume SI 1452-2 / 1452-3 layout
@@ -201,30 +234,7 @@ def loop(screen):
                 c = chr(ev.key_code).lower()
             except Exception:
                 pass
-            if c in ['q', 'ץ']:  # Quit
-                return
-            elif c in ['i', 'ת']:  # Initialize
-                init()
-            elif c in ['s', 'ד']:  # Solo on all tracks
-                toggle_all('s', True)
-            elif c in ['a', 'ש']:  # Solo off All tracks
-                toggle_all('s', False)
-            elif c in ['m', 'צ']:  # Mute on all tracks
-                toggle_all('m', True)
-            elif c in ['n', 'מ']:  # Mute off all tracks
-                toggle_all('m', False)
-            elif c in ['r', 'ר']:  # Record on all tracks
-                toggle_all('r', True)
-            elif c in ['e', 'ק']:  # Record off all tracks
-                toggle_all('r', False)
-            elif c in ['u', 'ו']:  # solo/mute/record off all tracks
-                toggle_all('msr', False)
-            elif c in ['o', 'ם']:  # Override mute on/off all tracks (software level)
-                override_mute = not override_mute
-                if not override_mute:
-                    need_screen_refresh = True
-                    screen.print_at('    ', screen.width - 4, 0, bg=bg_color)
-            elif c in ['h', 'י']:  # Help show/hide
+            if c in ['h', 'י']:  # Help show/hide
                 show_help = not show_help
                 if not show_help:
                     need_screen_refresh = True
@@ -233,6 +243,42 @@ def loop(screen):
                         new_controls[knob_start + k] = controls[knob_start + k]
                     for i, line in enumerate(help_text):
                         screen.print_at(re.sub(r'\S', ' ', line), help_x, help_y + i, bg=bg_color)
+            elif c in ['q', 'ץ']:  # Quit
+                return
+            elif c in ['i', 'ת']:  # Initialize
+                init()
+            elif c in ['s', 'ד']:  # Solo on all tracks
+                toggle_all('s', True)
+            elif c in ['a', 'ש']:  # solo off All tracks
+                toggle_all('s', False)
+            elif c in ['d', 'ג'] and external_led_mode:  # solo exclusive mode toggle
+                solo_exclusive = not solo_exclusive
+                if not solo_exclusive:
+                    need_screen_refresh = True
+                    screen.print_at(' ' * len(solo_exclusive_text), screen.width - len(solo_exclusive_text),
+                                    solo_exclusive_y, bg=bg_color)
+            elif c in ['m', 'צ']:  # Mute on all tracks
+                toggle_all('m', True)
+            elif c in ['n', 'מ']:  # mute off all tracks
+                toggle_all('m', False)
+            elif c in ['o', 'ם']:  # mute Override all tracks toggle (software level)
+                mute_override = not mute_override
+                if not mute_override:
+                    need_screen_refresh = True
+                    screen.print_at(' ' * len(mute_override_text), screen.width - len(mute_override_text),
+                                    mute_override_y, bg=bg_color)
+            elif c in ['r', 'ר']:  # Record on all tracks
+                toggle_all('r', True)
+            elif c in ['e', 'ק']:  # record off all tracks
+                toggle_all('r', False)
+            elif c in ['t', 'א'] and external_led_mode:  # record exclusive mode toggle
+                record_exclusive = not record_exclusive
+                if not record_exclusive:
+                    need_screen_refresh = True
+                    screen.print_at(' ' * len(record_exclusive_text), screen.width - len(record_exclusive_text),
+                                    record_exclusive_y, bg=bg_color)
+            elif c in ['u', 'ו']:  # solo/mute/record off all tracks
+                toggle_all('msr', False)
 
         if need_screen_refresh:
             screen.refresh()
